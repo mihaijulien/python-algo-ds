667e041cf3cab843f719c6a1c1cea22ab4cd3902
Julien Mihai <mihaijulien@gmail.com>
2021-12-27 23:01:26 +0200

    Add bubble sort

---
+++ tests/test_bubble.py
@@ -0,0 +1,15 @@
+import pytest
+from algorithms.bubble_sort import BubbleSort
+
+array_under_test = [9,8,7,6,5,4,3,2,1]
+expected = [1,2,3,4,5,6,7,8,9]
+
+bubble = BubbleSort()
+
+def test_bubble_sort():
+    assert bubble.sort(array_under_test) == expected
+
+def test_type_exception():
+	with pytest.raises(TypeError) as excinfo:
+		bubble.sort("wrong_type")
+	assert str(excinfo.value) == "Only lists are allowed"
